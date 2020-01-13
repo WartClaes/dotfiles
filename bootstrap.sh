9ca4e6def523b0d0863676274cb53ef67e0ff9f2
Wart Claes <wart.claes@gmail.com>
2020-01-13 15:16:26 +0100

    fix(bootstrap): fix bootstrap for zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,12 +22,12 @@ function doIt() {
 	fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" "==" "--force" -o "$1" "==" "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read reply"?This may overwrite existing files in your home directory. Are you sure? (y/n) ";
 	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	if [[ $reply =~ ^[Yy]$ ]]; then
 		doIt;
 	fi;
 fi;
