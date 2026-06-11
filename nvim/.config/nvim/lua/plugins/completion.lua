6f84d44a65ff3358c4cbc951012e9b96a4746093
Wart Claes <wart.claes@gmail.com>
2026-06-11 08:05:01 +0200

    feat(nvim): replace nvim-cmp with native 0.12 completion

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,3 +1,26 @@
+vim.o.completeopt = 'menu,menuone,noinsert,noselect,fuzzy'
+
+vim.keymap.set('i', '<Tab>', function()
+  return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>'
+end, { expr = true, silent = true })
+
+vim.keymap.set('i', '<S-Tab>', function()
+  return vim.fn.pumvisible() == 1 and '<C-p>' or '<S-Tab>'
+end, { expr = true, silent = true })
+
+vim.keymap.set('i', '<CR>', function()
+  if vim.fn.pumvisible() == 1 and vim.fn.complete_info()['selected'] ~= -1 then
+    return '<C-y>'
+  end
+  return '<CR>'
+end, { expr = true, silent = true })
+
+vim.keymap.set('i', '<C-Space>', '<C-x><C-o>', { silent = true })
+
+vim.schedule(function()
+  require('mini.pairs').setup()
+end)
+
 return {
   {
     'github/copilot.vim',
@@ -6,8 +29,8 @@ return {
       vim.g.copilot_enabled = false
       vim.g.copilot_no_tab_map = true
 
-      vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>');
-      vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>');
+      vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>')
+      vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>')
 
       vim.keymap.set('i', '<M-j>', 'copilot#Accept("\\<CR>")', {
         expr = true,
@@ -26,9 +49,8 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "hrsh7th/nvim-cmp",              -- Optional: For using slash commands and variables in the chat buffer
-      "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-      "stevearc/dressing.nvim",        -- Optional: Improves `vim.ui.select`
+      "nvim-telescope/telescope.nvim",
+      "stevearc/dressing.nvim",
     },
     cmd = {
       "CodeCompanion",
@@ -38,139 +60,10 @@ return {
     },
     opts = {
       strategies = {
-        chat = {
-          adapter = "anthropic"
-        },
-        inline = {
-          adapter = "anthropic",
-        },
-        agent = {
-          adapter = "anthropic",
-        },
+        chat = { adapter = "anthropic" },
+        inline = { adapter = "anthropic" },
+        agent = { adapter = "anthropic" },
       }
     },
   },
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'onsails/lspkind.nvim',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'saadparwaiz1/cmp_luasnip',
-      {
-        'L3MON4D3/LuaSnip',
-        dependencies = {
-          'rafamadriz/friendly-snippets',
-        },
-        config = function()
-          require('luasnip.loaders.from_vscode').lazy_load()
-        end
-      },
-      'windwp/nvim-autopairs',
-    },
-    config = function()
-      local cmp = require 'cmp'
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
-
-      -- Autopairs
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-      cmp.setup({
-          formatting = {
-            format = lspkind.cmp_format({
-                maxwidth = 50,
-                ellipsis_char = '...',
-                menu = {
-                  buffer = "[BUF]",
-                  cmdline = "[CMD]",
-                  luasnip = "[SNIP]",
-                  nvim_lsp = "[LSP]",
-                  path = "[PATH]",
-                },
-              })
-          },
-          window = {
-            documentation = vim.tbl_deep_extend('force', {},
-              cmp.config.window.bordered(),
-              {
-                max_height = 15,
-                max_width = 60,
-              }
-              ),
-          },
-          matching = {
-            disallow_prefix_unmatching = true,
-            disallow_partial_fuzzy_matching = true,
-            disallow_fullfuzzy_matching = true,
-          },
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          mapping = cmp.mapping.preset.insert({
-              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-              ['<C-f>'] = cmp.mapping.scroll_docs(4),
-              ['<C-Space>'] = cmp.mapping.complete(),
-              ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-              ['<C-e>'] = cmp.mapping.abort(),
-              ['<CR>'] = cmp.mapping.confirm({ select = false }),
-              ['<Tab>'] = cmp.mapping(function(fallback)
-                local col = vim.fn.col('.') - 1
-
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif luasnip.expand_or_locally_jumpable() then
-                  luasnip.expand_or_jump()
-                elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-                  fallback()
-                else
-                  cmp.complete()
-                end
-              end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end, { 'i', 's' })
-        }),
-      -- Installed sources
-      sources = cmp.config.sources({
-          { name = 'nvim_lsp', keyword_lenght = 2, max_item_count = 20 },
-          { name = 'luasnip',  keyword_lenght = 2, },
-          { name = "path", },
-        }, {
-          { name = "buffer", keyword_lenght = 4 },
-        }),
-    })
-
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-      sources = cmp.config.sources({
-          { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-        }, {
-          { name = 'buffer' },
-        })
-    })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-    })
-end
-    },
-  }
+}
